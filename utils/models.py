1155d49be46df6ec68540583e91e723c1443d4d1
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2021-12-02 10:57:23 +0530

    to cpu()

--- utils/models.py
+++ utils/models.py
@@ -389,7 +389,7 @@ class WordLSTM(nn.Module):
 
     def sample(self, features, start_tokens):
         sampled_ids = np.zeros((np.shape(features)[0], self.n_max))
-        sampled_ids[:, 0] = start_tokens.view(-1, )
+        sampled_ids[:, 0] = start_tokens.cpu().view(-1, )
         predicted = start_tokens
         embeddings = features
         embeddings = embeddings
