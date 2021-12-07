af26c71d6aceb1c2123a3addaeff3167480256b1
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2021-12-07 12:23:24 +0530

    update scalar_summary

--- utils/logger.py
+++ utils/logger.py
@@ -23,6 +23,7 @@ class Logger(object):
         # self.writer.scalar(tag, value, step)
         with self.writer.as_default():
             print(type(value))
+
             tf.summary.scalar(tag, value.device('cpu'), step)
 
     def image_summary(self, tag, images, step):
