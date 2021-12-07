e9c02bfc4198943a721074c9d4d74b036bb9941f
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2021-12-07 11:39:16 +0530

    update scalar_summary

--- utils/logger.py
+++ utils/logger.py
@@ -20,7 +20,9 @@ class Logger(object):
         """Log a scalar variable."""
         # summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
         # self.writer.add_summary(summary, step)
-        self.writer.scalar(tag, value, step)
+        # self.writer.scalar(tag, value, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step)
 
     def image_summary(self, tag, images, step):
         """Log a list of images."""
